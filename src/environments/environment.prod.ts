e0192205c581cf2335fb003f65c740d0223c885d
daniel.perez <daniel.perez@almundo.com>
2019-09-16 17:32:49 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDTFUfqvPpsFh7rdWtaecm5pni0GkOXowI",
+    authDomain: "list-players.firebaseapp.com",
+    databaseURL: "https://list-players.firebaseio.com",
+    projectId: "list-players",
+    storageBucket: "",
+    messagingSenderId: "332022011954",
+    appId: "1:332022011954:web:ebd90c95454b493b19c42b"
+  }
 };
